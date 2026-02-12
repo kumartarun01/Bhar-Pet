dd5a5f98810d80fb843c8781c6650980bb78d85e
Tarun Kumar <jaatrony84@gmail.com>
2026-02-12 12:21:25 +0530

    Initial Commit

---
+++ Bhar Pet/Bhar_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bhar_PetApp.swift
+//  Bhar Pet
+//
+//  Created by iMac1 on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Bhar_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bhar Pet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bhar Pet
+//
+//  Created by iMac1 on 12/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
